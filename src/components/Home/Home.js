418dc0d478cdcdc8d5bbd359be169ab625d23e16
onuruci <onur.yuce@metu.edu.tr>
2022-06-11 15:18:08 +0300

    main generator

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./Home.css"
 import Header from "../Header";
 
+import MainGenerator from "../MainGenerator/MainGenerator";
+
 const Home = () => {
+
     return(
         <div className="home-super-class">
             
             <Header></Header>
             <div className="main-content">
-
+                <MainGenerator/>
             </div>
             <div className="footer">
                 
--- src/components/MainGenerator/MainGenerator.js
+++ src/components/MainGenerator/MainGenerator.js
@@ -1,10 +1,47 @@
 import React from "react";
-
+import { useState, useRef } from "react";
+import QRCode from "qrcode.react";
+import './MainGenerator.css'
 
 const MainGenerator = () => {
+
+    const [url, setUrl] = useState("https://www.qrcode-monkey.com/");
+    const qrRef = React.useRef();
+
+    const qrCode = (
+        <QRCode
+          id="qrCodeElToRender"
+          size={330}
+          value={url}
+          bgColor="white"
+          fgColor="#141926"
+          level="Q"
+        />
+    );
+
+    const downloadQRCode = () => {
+        let canvas = qrRef.current.querySelector("canvas");
+        let image = canvas.toDataURL("image/png");
+        let anchor = document.createElement("a");
+        anchor.href = image;
+        anchor.download = `qr-code.png`;
+        document.body.appendChild(anchor);
+        anchor.click();
+        document.body.removeChild(anchor);
+        setUrl("");
+    };
+
     return(
-        <div>
-            QR Code Generator
+        <div id="main-generator">
+            <div className="qr-editor">
+                <input type="text" value={url} onChange={e => setUrl(e.target.value)} />
+                <button onClick={() => downloadQRCode() }>Download QR</button>
+            </div>
+            <div className="qr-container">
+                <div className="qr" ref={qrRef}>
+                    {qrCode}
+                </div>
+            </div>
         </div>
     );
 };

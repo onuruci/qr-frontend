313ad702705f99112802506c9f5f4d9b1eeacfff
tunahan dündar <84476929+immpeccable@users.noreply.github.com>
2022-06-11 14:25:07 +0300

    header
    header added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./Home.css"
 
 const Home = () => {
     return(
-        <div>
-            Home
+        <div className="home-super-class">
+            <div className="header">
+                <div className="left-side">
+                    <tr className="qr-code-text">qrcode</tr>lemon
+                </div>
+                <div className="right-side">
+
+
+                </div>
+            </div>
+
+            <div className="main-content">
+
+            </div>
+            <div className="footer">
+                
+            </div>
         </div>
     );
 };
